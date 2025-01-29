5f5b5f91f7b05ff338b7a6eef2203a43a8e4100f
itayasaf1990xd <89840492+itayasaf1990xd@users.noreply.github.com>
2025-01-29 22:03:29 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
-import { useState } from 'react'
 import './App.css'
 import VolumControllApp from './Components/VolumControllApp/VolumControllApp'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      {/* <VoiceRecognitionApp /> */}
       <VolumControllApp />
     </>
   )
